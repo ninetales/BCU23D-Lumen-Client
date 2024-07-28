a4121feddffae020a4cf4d831b758311144b7bab
N1N3T4L3S <6765614+ninetales@users.noreply.github.com>
2024-07-28 17:42:47 +0200

    :lipstick: styling

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-import transactionHandler from '../utils/TransactionHandler';
+import transactionHandler from '../utils/transactionHandler.js';
 
 export const Block = ({ block }) => {
   const timestamp = new Date(block.timestamp);
   const [transactions, setTransactions] = useState([]);
+  const [toggle, setToggle] = useState(false);
 
   useEffect(() => {
     setTransactions(transactionHandler({ transactions: block.transactions }));
@@ -12,7 +13,7 @@ export const Block = ({ block }) => {
 
   return (
     <li className="block">
-      <div className="block__header">
+      <div className="block__header" onClick={() => setToggle(!toggle)}>
         <div className="block__header-info">
           <span>
             {block.blockIndex === 1
@@ -22,9 +23,9 @@ export const Block = ({ block }) => {
         </div>
         <span>Mined: {timestamp.toUTCString()}</span>
       </div>
-      <div className="block__body">
+      <div className={`block__body ${toggle ? 'toggled' : ''}`}>
         <div className="block__details block__section">
-          <span>Block Details:</span>
+          <h3>Block Details:</h3>
           <ul className="block__list">
             <li>
               <span>Last hash:</span>
@@ -51,12 +52,19 @@ export const Block = ({ block }) => {
 
         {transactions.length > 0 && (
           <div className="block__transactions block__section">
-            <span>Transactions:</span>
+            <h3>Transactions:</h3>
             <ul className="block__list">
               {transactions.map((transaction, index) => {
                 const timestamp = new Date(transaction.timestamp);
                 return (
-                  <li key={index} className="block__transaction">
+                  <li
+                    key={index}
+                    className={`block__transaction ${
+                      transaction.sender === 'reward-address'
+                        ? 'block__transaction-reward'
+                        : 'block__transaction-regular'
+                    }`}
+                  >
                     <ul className="block__transaction-data">
                       <li>
                         <span>Sender:</span>
