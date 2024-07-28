8c10bf6a31d43d84da55c38c653b8edc42753731
N1N3T4L3S <6765614+ninetales@users.noreply.github.com>
2024-07-28 22:23:15 +0200

    :lipstick: style: mining page

--- src/components/BlockMiner.jsx
+++ src/components/BlockMiner.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import Block from '../services/block';
+import Wallet from '../services/wallet';
+import { useEffect, useState } from 'react';
 
 export const BlockMiner = () => {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = Object.entries(await Wallet.transactions()).map(
+        ([, data]) => data
+      );
+
+      setTransactions(response);
+    })();
+  }, []);
+
   const mineBlock = async (e) => {
     e.preventDefault();
     Block.mine();
@@ -9,13 +23,13 @@ export const BlockMiner = () => {
 
   return (
     <>
-      <button
-        onClick={(e) => {
-          mineBlock(e);
-        }}
-      >
-        Mine Block
-      </button>
+      {transactions.length > 0 ? (
+        <button onClick={(e) => mineBlock(e)}>Mine Block</button>
+      ) : (
+        <div class="mine-message">
+          <span>There are no transactions to mine.</span>
+        </div>
+      )}
     </>
   );
 };
